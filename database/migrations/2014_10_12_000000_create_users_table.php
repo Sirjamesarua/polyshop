08ed62426d90f52dea4af6f4588613c6475a76ac
sirjamesarua <dev.sirjames@gmail.com>
2022-04-04 08:25:09 -0700

    setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->bigInteger('phonenumber');
             $table->string('location');
+            $table->string('department');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
