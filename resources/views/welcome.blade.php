e29dea9a8f61de2b8f1333c82a0e3847af2011aa
sirjamesarua <dev.sirjames@gmail.com>
2022-04-04 16:29:14 -0700

    setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
             <!--TRENDING-->
             <!--MAIN-->
             <div class="main-container w3-center border-center">
-                    <div class="main w3-center borders-center w3-round-xxxlarge" style="font-size: 115%;>
+                    <div class="main w3-center borders-center w3-round-xxxlarge" style="font-size: 115%;">
                     
                                 <br />
                                         <b class="w3-left w3-text-black">TRENDING</b>
