04af45e3645a91f6fb069818e63a916fdbc1078f
sirjamesarua <dev.sirjames@gmail.com>
2022-04-04 17:27:33 -0700

    setup

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -7,7 +7,7 @@
 			<!--VIEWED CATEGORIZE-->
 
 			<!--BACK BTN-->
-            <div class="w3-margin"><center><u><a href="{{ url()->previous() }}" class="btn btn-default" style="color:blue; font-size: 115%;"><span class="fa fa-arrow-left"></span>Back</a></u></center></div>
+            <div class="w3-margin"><center><a href="{{ url()->previous() }}" class="btn btn-default" style="color:blue; font-size: 115%;"><span class="fa fa-arrow-left"></span><u>Back</u></a></center></div>
 
 			
 			<!--MAIN-->
